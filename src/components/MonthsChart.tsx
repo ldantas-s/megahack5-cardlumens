8cdac1a057512b6315f5097a03e08c4af4180628
ldantas-s <lucianostudant@gmail.com>
2020-11-14 09:43:40 -0300

    Add MonthsChart

---
+++ src/components/MonthsChart.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Line, defaults } from 'react-chartjs-2';
+import { RiFlag2Fill } from 'react-icons/ri';
+
+import '../assets/css/compenents/monthsChart.css';
+
+// interface MonthsChartInterface {
+//   months: Array<{
+//     monthName: string;
+//   }>;
+// }
+
+function MonthsChart() {
+  let data = {
+    labels: ['Nov', 'Dez', 'Jan', 'Fev'],
+    datasets: [
+      {
+        data: [10, 59, 39, 20],
+        backgroundColor: ['rgba(255, 255, 255, 1)'],
+        borderColor: ['rgba(0,102,153,.5)'],
+        borderWidth: 4,
+      },
+    ],
+  };
+  return (
+    <section className="monthsChart">
+      <h2 className="title2">Resumo</h2>
+      <div className="monthsChart-secChart">
+        <header className="secChart-header">
+          <h3 className="title2">
+            <span className="secChart-header__span">Valor a pagar:</span> R$
+            39,00
+          </h3>
+          <div className="secChart-header-flags">
+            <RiFlag2Fill size={18} />
+            <RiFlag2Fill size={18} />
+          </div>
+        </header>
+
+        <Line data={data} width={100} height={80} />
+      </div>
+    </section>
+  );
+}
+
+export default MonthsChart;
--- src/pages/CardCpfl.tsx
+++ src/pages/CardCpfl.tsx
@@ -1,15 +1,29 @@
-import React from "react";
-import PopupFlag from "../components/PopupFlag";
+import React, { useState } from 'react';
+import PopupFlag from '../components/PopupFlag';
 
-import "../assets/css/pages/cardCpfl.css";
+import '../assets/css/pages/cardCpfl.css';
+import MonthsChart from '../components/MonthsChart';
 
 function CardCpfl() {
+  const [infoClient] = useState({
+    months: [{ monthName: 'Mai' }],
+  });
+
   return (
     <section className="cardCpfl">
-      <h1 className="title1 cardCpfl__title">CardCPFL</h1>
+      <h1 className="title1 cardCpfl__title">QrLumens</h1>
 
       <PopupFlag />
-      <h2 className="title2">Resumo</h2>
+
+      <MonthsChart />
+
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.cpfl.com.br/releases/Paginas/cpfl-energia-lanca-conta-facil-servico-que-explica-para-o-cliente-todos-os-detalhes-da-conta.aspx"
+      >
+        Mais Detalhes
+      </a>
     </section>
   );
 }
